37b1f3dfdfb50bb09d541be621761f3099ea2fbe
David Blurton <david.blurton@gmail.com>
2014-07-15 16:14:13 +0100

    Add bar chart definition

---
+++ index.js
@@ -0,0 +1,44 @@
+module.exports = {
+  bar: function(data, xAxisLabel, yAxisLabel) {
+    return {
+      chart: {
+        type: 'column'
+      },
+      title: {
+        text: ''
+      },
+      xAxis: {
+        title: {
+          text: xAxisLabel
+        },
+        categories: data.map(function(x) {
+          return x.name;
+        }),
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: yAxisLabel
+        }
+      },
+      legend: {
+        enabled: false
+      },
+      credits: {
+        enabled: false
+      },
+      colors: ['#73B02D'],
+      plotOptions: {
+        column: {
+          pointPadding: 0.2,
+          borderWidth: 0
+        }
+      },
+      series: [{
+        data: data.map(function(x) {
+          return +x.value;
+        })
+      }]
+    };
+  }
+};
